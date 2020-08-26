859a789c1e3d726055318df8a45c42189b5d736a
Guilherme Guerra <guilherme.ga@gmail.com>
2020-08-26 19:26:59 -0300

    All in one

--- examples/publish.el
+++ examples/publish.el
@@ -9,10 +9,25 @@
 
 (use-package templatel)
 (require 'blorg)
+(use-package htmlize)
+(setq org-html-htmlize-output-type 'css)
 
 ;; Generate blog posts
 (blorg-cli
  :base-dir: './'
- :input-pattern ".*\\.org$"
+ :input-pattern "posts/.*\\.org$"
  :template "post.html"
  :output "../output/posts/{{ slug }}.html")
+
+;; Generate pages
+(blorg-cli
+ :input-pattern "pages/.*\\.org$"
+ :template "page.html"
+ :output "../output/{{ slug }}/index.html")
+
+;; Generate posts summary
+(blorg-cli
+ :input-pattern "posts/.*\\.org$"
+ :input-aggregate #'blorg-input-aggregate-all
+ :template "blog.html"
+ :output "../output/blog/index.html")
