64bc45881ba24cbbb98715fd8a8eda30b84f820f
Lincoln de Sousa <lincoln@clarete.li>
2021-08-22 11:59:33 -0400

    fix RE that filtered out paths with dots (.emacs.d included :/)

--- weblorg.el
+++ weblorg.el
@@ -374,7 +374,7 @@ Parameters in ~OPTIONS~:
      (puthash :base-dir base-dir route)
      (puthash :theme theme route)
      (puthash :input-pattern (weblorg--get opt :input-pattern "**/*") route)
-     (puthash :input-exclude (weblorg--get opt :input-exclude (regexp-opt '("/." "/.." "/output"))) route)
+     (puthash :input-exclude (weblorg--get opt :input-exclude (regexp-opt '("../" "/output"))) route)
      (puthash :input-filter (weblorg--get opt :input-filter) route)
      (puthash :input-parser #'identity route)
      (puthash :input-aggregate #'identity route)
