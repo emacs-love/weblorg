3f788ee8e2af7c8871d33d3449940879f67f343e
Nan Zhong <me@nanzho.ng>
2021-09-20 14:21:59 -0400

    Add support for org-roam input sources

--- weblorg.el
+++ weblorg.el
@@ -549,6 +549,70 @@ last."
 
 
 
+;; ---- Input Source: org-roam ----
+
+(defvar weblorg--org-id-updated nil
+  "Internal state representing if org id mapping have been updated.")
+
+(defvar weblorg--org-roam-db-synced nil
+  "Internal state representing if org-roam db has been synced.")
+
+(defun weblorg--input-source-org-roam-nodes (&optional filter render-org)
+  "Computes an alist for each org-roam file node that matches FILTER, optionally
+rendering the html if RENDER-ORG is non-nil."
+  ;; Make sure we are starting off with a sync org-roam db.
+  (unless weblorg--org-roam-db-synced
+    (org-roam-db-sync)
+    (setq weblorg--org-roam-db-synced t))
+
+  (let* ((org-roam-files (sort (mapcar (lambda (file)
+                                         `(("file" . ,(nth 0 file))
+                                           ("atime" . ,(nth 1 file))
+                                           ("mtime" . ,(nth 2 file))))
+                                       (org-roam-db-query [:select [file atime mtime] :from files]))
+                               (lambda (a b)
+                                 (string< (cdr (assoc "file" a))
+                                          (cdr (assoc "file" b)))))))
+
+    ;; Update id locations based on the org-roam-files.
+    (unless weblorg--org-id-updated
+      (org-id-update-id-locations (mapcar (lambda (file) (cdr (assoc "file" file))) org-roam-files))
+      (setq weblorg--org-id-update t))
+
+    (delq nil (mapcar (lambda (file)
+                        (let ((file-path (cdr (assoc "file" file))))
+                          (org-roam-with-temp-buffer file-path
+                            (let* ((slug (weblorg--slugify (file-name-sans-extension (file-name-nondirectory file-path))))
+                                   (node (org-roam-node-at-point))
+                                   (tags (car (org-roam-db-query [:select [tag] :from tags :where (= node-id $s1)]
+                                                                 (org-roam-node-id node))))
+                                   (backlinks (mapcar (lambda (node)
+                                                        (let* ((source-node (org-roam-backlink-source-node node))
+                                                               (source-slug (weblorg--slugify (file-name-sans-extension (file-name-nondirectory (org-roam-node-file source-node)))))
+                                                               (source-title (org-roam-node-title source-node)))
+                                                          `(("slug" . ,source-slug)
+                                                            ("title" . ,source-title))))
+                                                      (org-roam-backlinks-get node)))
+                                   (keywords (if render-org (weblorg--parse-org (buffer-string) file-path)
+                                               `(("title" . ,(org-roam-node-title node))))))
+                              (weblorg--prepend keywords (cons "backlinks" backlinks))
+                              (weblorg--prepend keywords (cons "tags" tags))
+                              (weblorg--prepend keywords (cons "slug" slug))
+                              (if filter (if (funcall filter keywords) keywords
+                                           nil)
+                                keywords)))))
+                      org-roam-files))))
+
+(defun weblorg-input-source-org-roam-nodes (&optional filter)
+  "Find all org-roam file nodes that match the optional FILTER"
+  (mapcar (lambda (node)
+            `(("node" . ,node)))
+          (weblorg--input-source-org-roam-nodes filter t)))
+
+(defun weblorg-input-source-org-roam-nodes-agg (&optional filter)
+  "Aggregate all org-roam file nodes that match the optional FILTER."
+  `((("nodes" . ,(weblorg--input-source-org-roam-nodes filter)))))
+
 ;; ---- Input Source: autodoc ----
 
 (defun weblorg-input-source-autodoc (pattern)
@@ -782,13 +846,23 @@ default templates."
 This function also installs an Org-Mode link handler `url_for'
 that is accessible with the same syntax as the template filter."
   (let ((site (gethash :site route))
-        (env (gethash :template-env route)))
+        (env (gethash :template-env route))
+        (name (gethash :name route)))
     ;; Install link handlers
     (org-link-set-parameters
      "anchor"
      :export (lambda(path desc _backend)
                (format "<a href=\"#%s\">%s</a>" path desc)))
     (org-link-set-parameters
+     "id"
+     :export (lambda (id desc _backend)
+               (format "<a href=\"%s\">%s</a>"
+                       (weblorg--url-for (format "%s,slug=%s"
+                                                 name
+                                                 (weblorg--slugify (file-name-sans-extension (file-name-nondirectory (org-id-find-id-file id)))))
+                                         site)
+                       desc)))
+     (org-link-set-parameters
      "url_for"
      :export (lambda(path desc _backend)
                (format "<a href=\"%s\">%s</a>" (weblorg--url-for path site) desc)))
